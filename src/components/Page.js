bd4124f0e02c9cdbf327de5b662d9275b859305d
anti-matt-er <m.burrows@live.co.uk>
2020-08-14 01:43:32 +0100

    Fix caret positioning and clean markdown logic

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,28 +1,57 @@
 import React, { Component } from "react";
 import ContentEditable from "react-contenteditable";
+import Rangy from "rangy";
+import RangyTextRange from "rangy/lib/rangy-textrange";
 import Typer from "../modules/Typer";
 import "../styles/Page.scss";
 import Filler from "../resources/filler.md";
-const FillerText = Typer.normalize(Filler);
+
+const contentChangeTimeout = 500;
 
 class Page extends Component {
   constructor() {
     super();
     this.contentEditable = React.createRef();
+    this.typer = new Typer();
+    this.typer.setMd(Filler);
+    this.previousHtml = this.typer.getHtml();
+    this.contentChangeTimer = null;
     this.state = {
-        previousHtml: FillerText,
-        html: FillerText
+        html: this.previousHtml
     };
+    Rangy.init();
   }
 
   handleChange(e) {
     var newContent = e.target.value;
-    if (newContent == this.state.previousHtml) return;
-    this.state.previousHtml = newContent;
-    var normalizedHtml = Typer.normalize(newContent);
-    if (newContent == normalizedHtml) return;
-    this.setState({ html: normalizedHtml });
+    if (newContent == this.previousHtml) {
+      return;
+    }
+
+    clearTimeout(this.contentChangeTimer);
+    this.contentChangeTimer = setTimeout(this.handleContent.bind(this), contentChangeTimeout, newContent);
   };
+
+  handleContent(content) {
+    this.previousHtml = content;
+    this.typer.setHtml(content);
+    if (content == this.typer.getHtml()) {
+      return;
+    }
+
+    let node = this.contentEditable.current;
+    let caret = Rangy.getSelection().getRangeAt(0).toCharacterRange(node);
+
+    this.setState({
+      html: this.typer.getHtml()
+    }, () => {
+      let sel = Rangy.getSelection();
+      let range = sel.getRangeAt(0);
+      range.selectCharacters(node, caret.start, caret.start);
+      sel.setSingleRange(range);
+      node.focus();
+    });
+  }
 
   render() {
     return (
--- src/modules/Typer.js
+++ src/modules/Typer.js
@@ -1,30 +1,95 @@
 import SanitizeHtml from "sanitize-html";
 import Marked from "marked";
 import Turndown from "turndown";
+Marked.setOptions({
+    gfm: true,
+    breaks: true,
+    smartypants: true
+});
 const turndown = new Turndown();
+const turndownPluginGfm = require('turndown-plugin-gfm');
+turndown.use(turndownPluginGfm.gfm);
 const sanitizePrefs = {
-    allowedTags: [ 'em', 'strong', 'ul', 'ol', 'li', 'p', 'blockquote', 'h1', 'h2', 'h3', 'h4', 'hr']
+    allowedTags: [ 'em', 'strong', 'ul', 'ol', 'li', 'p', 'br', 'blockquote', 'h1', 'h2', 'h3', 'h4', 'hr'],
+    allowedAttributes: {}
 }
+const caretCode = '\uffff';
 
 class Typer {
-    static sanitize(html) {
-        return SanitizeHtml(html, sanitizePrefs);
+    constructor() {
+        this.html = '';
+        this.md = '';
     }
 
-    static markdown(text) {
-        return Marked(text);
+    setHtml(html) {
+        this.html = html;
+        this.sanitize();
+        this.setMd(this.htmlToMarkdown(this.html));
+        console.log(this.md);
     }
 
-    static markup(html) {
+    setMd(md) {
+        this.md = md;
+        this.html = this.markdownToHtml(md);
+        this.sanitize();
+    }
+
+    getHtml() {
+        return this.html;
+    }
+
+    getMd() {
+        return this.md;
+    }
+
+    sanitize() {
+        this.html = SanitizeHtml(this.html, sanitizePrefs);
+        //this.fixTags();
+    }
+
+    markdownToHtml(md) {
+        return Marked(md);
+    }
+
+    htmlToMarkdown(html) {
         return turndown.turndown(html);
     }
 
-    static normalize(content) {
-        return this.markdown(
-            this.markup(
-                this.sanitize(content)
-            )
-        );
+    fixTags() {
+        this.html = this.html.replace(/(?<=\S)</g, ' <');
+        this.html = this.html.replace(/>(?=\S)/g, '> ');
+    }
+
+    setHtmlCaret(pos) {
+        this.setHtml([
+            this.html.slice(0, position),
+            caretCode,
+            this.html.slice(position)
+        ].join(''));
+    }
+
+    setMdCaret(pos) {
+        this.setMd([
+            this.md.slice(0, position),
+            caretCode,
+            this.md.slice(position)
+        ].join(''));
+    }
+
+    getHtmlCaret() {
+        var pos = this.html.indexOf(caretCode);
+        if (pos == -1) return 0;
+        this.html.replace(caretCode, '');
+        this.setHtml(this.html);
+        return pos;
+    }
+
+    getMdCaret() {
+        var pos = this.md.indexOf(caretCode);
+        if (pos == -1) return 0;
+        this.md.replace(caretCode, '');
+        this.setMd(this.md);
+        return pos;
     }
 }
 
